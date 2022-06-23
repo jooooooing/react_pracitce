9e87d404359970abfe08242c195f554c9fedd8ef
jooooooing <leegun1018@naver.com>
2022-06-23 17:39:06 +0900

    컴포넌트, props

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import "./App.css";
-import { Component } from "react";
 
-class App extends Component {
-  render() {
-    return <div className="App">
-      Hello, React!
-      <Subject></Subject>
-      <TOC></TOC>
-      </div>;
-  }
+function Header(props) {
+  console.log("props", props, props.title);
+  return (
+    <header>
+      <h1>
+        <a href="/">{props.title}</a>
+      </h1>
+    </header>
+  );
 }
 
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <li><a href="1.html">HTML</a></li>
-        <li><a href="2.CSS">CSS</a></li>
-        
-      </nav>
-    )
+function Nav(props) {
+  const lis = []
+  for(let i =0; i < props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
   }
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  );
 }
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>web</h1>
-        <h2>world wide web</h2>
-      </header>
-    );
-  }
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
+function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is..." },
+    { id: 2, title: "css", body: "css is..." },
+    { id: 3, title: "javscript", body: "javascript is..." },
+  ];
+  return (
+    <div className="App">
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
+    </div>
+  );
 }
 export default App;
