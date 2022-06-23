f448128b245bf5d07654a075aa129ddc63c7662b
jooooooing <leegun1018@naver.com>
2022-06-23 15:49:04 +0900

    리액트 기본세팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div className="App">
+      Hello, React!
+      <Subject></Subject>
+      <TOC></TOC>
+      </div>;
+  }
 }
 
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <li><a href="1.html">HTML</a></li>
+        <li><a href="2.CSS">CSS</a></li>
+        
+      </nav>
+    )
+  }
+}
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>web</h1>
+        <h2>world wide web</h2>
+      </header>
+    );
+  }
+}
 export default App;
